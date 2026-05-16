c949d700b0cacec3784afc206f67db0f364d680c
gunguna1 <karan26504@gmail.com>
2026-05-16 15:23:44 +0530

    Implement JWT authentication and protected routes

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  try {
+    // check token
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+
+      token = req.headers.authorization.split(" ")[1];
+
+      // verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // get user from token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+
+    } else {
+      return res.status(401).json({
+        message: "Not authorized, no token",
+      });
+    }
+
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token failed",
+    });
+  }
+};
+
+export default protect;
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+//temporary
+import protect from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
@@ -11,9 +15,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running");
+});
+
+//temporary protected route to test auth middleware
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
 });
 
 const PORT = process.env.PORT || 5000;
